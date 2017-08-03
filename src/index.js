1478f217fe70ef44879fd47b9ed788e14928d4b1
happylynx <czerny.jakub@gmail.com>
2017-08-03 04:33:00 +0200

    core: flow output at the end
    * flow output is printed at the end of webpack output
    * flow output is prefixed in verbose mode

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ declare var require: {
     main: typeof module
 }
 
+const EOL = require('os').EOL
 const spawn = require('child_process').spawn
 
 interface OptionalOptions {
@@ -26,6 +27,12 @@ interface Options {
     verbose: boolean
 }
 
+type CheckResult = {
+    successful: boolean,
+    stdout: string,
+    stderr: string
+}
+
 function FlowWebpackPlugin(options: OptionalOptions) {
     options = options || {}
     this.options = applyOptionsDefaults(options)
@@ -36,42 +43,87 @@ function FlowWebpackPlugin(options: OptionalOptions) {
     }
 }
 
+function prefixLines(prefix: string, lines: string): string {
+    return lines.split(/\r?\n/)
+        .map(line => prefix + ': ' + line)
+        .join(EOL) + EOL
+}
+
 FlowWebpackPlugin.prototype.apply = function(compiler) {
     const plugin = this
 
-    let errorToReport = undefined
+    let flowResult: CheckResult
+    let flowExecutionError: any = undefined;
 
     const runCallback = failOnError =>
         (compiler, callback) => {
-            errorToReport = null
             flowCheck()
-                .then(successful => {
-                    if (!successful) {
-                        errorToReport = new Error('Flow validation.')
+                .then(result => {
+                    if (!result.successful && failOnError) {
+                        const details = plugin.options.printFlowOutput ? (EOL + formatFlowOutput(result)) : ''
+
+                        /*
+                         * argument passed to callback() causes webpack to immediately stop, even in watch mode,
+                         * don't emit assets, and set return code to 1
+                         */
+                        callback('Flow validation failed.' + details)
+                        return
                     }
-                    (!successful && failOnError) ? callback(errorToReport) : callback()
+                    flowResult = result
+                    callback()
                 })
                 .catch(error => {
-                    errorToReport = error
-                    failOnError ? callback(error) : callback()
+                    flowExecutionError = error
+                    failOnError ? callback('Flow execution failed. ' + error) : callback()
                 })
         }
 
     compiler.plugin('compilation', compilation => {
-        if (!!errorToReport) {
-            compilation.errors.push(errorToReport)
+        if (flowExecutionError) {
+            /*
+             * Passed object will be printed at the end of the compilation in red
+             * (unless specified otherwise), webpack still emits assets,
+             * return code will still 0.
+             */
+            compilation.errors.push('Flow execution: ' + flowExecutionError)
+            return
         }
+
+        if (flowResult.successful) {
+            return
+        }
+
+        const details = plugin.options.printFlowOutput ? (EOL + formatFlowOutput(flowResult)) : ''
+        compilation.errors.push('Flow validation' + details)
     })
 
+    /*
+     * callbacks chosen because it is required
+     * * to be done before or at time of 'compilation callback' - to avoid expensive compilation when type error present
+     * * hook needs to be asynchronous
+     */
     compiler.plugin('run', runCallback(plugin.options.failOnError))
     compiler.plugin('watch-run', runCallback(plugin.options.failOnErrorWatch))
 
-    function flowCheck() {
+    function formatFlowOutput(result: CheckResult): string {
+        return prefixIfVerbose('flow stdout', result.stdout)
+            + prefixIfVerbose('flow stderr', result.stderr)
+    }
+
+    function prefixIfVerbose(prefix: string, lines: string): string {
+        return plugin.options.verbose ? prefixLines(prefix, lines) : lines
+    }
+
+    function flowCheck(): Promise<CheckResult> {
         return new Promise((resolve, reject) => {
             log(`spawning flow`)
             const flowProcess = spawn(plugin.options.flowPath, plugin.options.flowArgs, {
-                stdio: plugin.options.printFlowOutput ? 'inherit' : 'ignore'
+                stdio: getStdioOptions()
             })
+            let stdout: string = ''
+            let stderr: string = ''
+            plugin.options.printFlowOutput && flowProcess.stdout.on('data', data => stdout += data.toString())
+            plugin.options.printFlowOutput && flowProcess.stderr.on('data', data => stderr += data.toString())
             let resolved = false
             flowProcess.on('error', error => {
                 pluginPrint('flow execution failed. Please make sure that the `flowPath` option is correctly set.'
@@ -84,9 +136,17 @@ FlowWebpackPlugin.prototype.apply = function(compiler) {
                 if (resolved) {
                     return
                 }
-                resolve(exitCode == 0)
+                resolve({
+                    successful: exitCode === 0,
+                    stdout,
+                    stderr
+                })
             })
         })
+    }
+
+    function getStdioOptions(): string {
+        return plugin.options.printFlowOutput ? 'pipe' : 'ignore'
     }
 
     function log(...messages) {
@@ -138,26 +198,16 @@ function isArrayOfStrings(object: mixed) {
         && object.every(item => isString(item))
 }
 
-function applyOptionsDefaults(options: OptionalOptions): Options {
-    if (!('failOnError' in options)) {
-        options.failOnError = false
+function applyOptionsDefaults(optionalOptions: OptionalOptions): Options {
+    const defaultOptions: Options = {
+        failOnError: false,
+        failOnErrorWatch: false,
+        printFlowOutput: true,
+        flowPath: getLocalFlowPath(),
+        flowArgs: ['--color=always'],
+        verbose: false
     }
-    if (!('failOnErrorWatch' in options)) {
-        options.failOnErrorWatch = false
-    }
-    if (!('printFlowOutput' in options)) {
-        options.printFlowOutput = true
-    }
-    if (!('flowPath' in options)) {
-        options.flowPath = getLocalFlowPath()
-    }
-    if (!('flowArgs' in options)) {
-        options.flowArgs = []
-    }
-    if (!('verbose' in options)) {
-        options.verbose = false
-    }
-    return (options: any)
+    return (Object.assign({}, defaultOptions, optionalOptions): any)
 }
 
 class FlowWebpackPluginError extends Error {}
